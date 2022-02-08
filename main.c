b8caeca0e8d3c7daa365e8c987a8743a90176e01
daveriesz <dave@riesz.net>
2022-02-07 21:54:36 -0800

    made it work

--- main.c
+++ main.c
@@ -1,19 +1,40 @@
+/*
+ * main.c - (C) David Riesz 2022
+ *
+ * This program test/control a radio using Hamlib.
+ * It takes commands in interactive mode as well as
+ * from command line options.
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published
+ *   by the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with this program; if not, write to the Free Software Foundation,
+ *   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
+#include <unistd.h>
 #include <locale.h>
 
 #include "rigsync.h"
 
 void read_args(int argc, char **argv);
-void msleep(int ms);
-void nsleep(int ns);
 
 struct rs_rig_state { freq_t freq; rmode_t mode; pbwidth_t width; };
-void rs_rig_read_state (RIG *rig, struct rs_rig_state *state);
-void rs_rig_write_state(RIG *rig, struct rs_rig_state *state);
-int rs_rig_state_cmp(struct rs_rig_state *statea, struct rs_rig_state *stateb);
+void   rs_rig_read_state (RIG *rig, struct rs_rig_state *state);
+void   rs_rig_write_state(RIG *rig, struct rs_rig_state *state);
+int    rs_rig_state_cmp(struct rs_rig_state *statea, struct rs_rig_state *stateb);
+void   rs_rig_state_cpy(struct rs_rig_state *state_dest, struct rs_rig_state *state_orig);
 
 int main(int argc, char **argv)
 {
@@ -21,7 +42,7 @@ int main(int argc, char **argv)
   rig_model_t modela, modelb;
   hamlib_port_t porta, portb;
   struct rs_rig_state statea, stateb;
-  struct rs_rig_state statep;
+  struct rs_rig_state stateap, statebp;
   char str[1024];
   int retval;
   int startup = 1;
@@ -32,21 +53,21 @@ int main(int argc, char **argv)
   
   read_args(argc, argv);
 
-  modela = RIG_MODEL_NETRIGCTL; /* rigctl server  */
-  modelb = RIG_MODEL_FT857;     /* Yaesu FT-857 */
+  modela = RIG_MODEL_FT857;     /* Yaesu FT-857 */
+  modelb = RIG_MODEL_NETRIGCTL; /* rigctl server  */
 
   riga = rig_init(modela);
   rigb = rig_init(modelb);
 
-  strncpy(riga->state.rigport.pathname, "localhost:4532", FILPATHLEN - 1);
 
-  rigb->state.rigport.parm.serial.rate = 38400;
-  strncpy(rigb->state.rigport.pathname, "/dev/ttyUSB0", FILPATHLEN - 1);
+  riga->state.rigport.parm.serial.rate = 38400;
+  strncpy(riga->state.rigport.pathname, "/dev/ttyUSB0", FILPATHLEN - 1);
+  strncpy(rigb->state.rigport.pathname, "localhost:4532", FILPATHLEN - 1);
 
   retval = rig_open(riga);
-  if(retval != RIG_OK) { fprintf(stderr, "*** Could not open NETRIGCTL.\n"); exit(1); }
+  if(retval != RIG_OK) { fprintf(stderr, "*** Could not open FT857.\n"); exit(1); }
   retval = rig_open(rigb);
-  if(retval != RIG_OK) { fprintf(stderr, "*** Could not oien FT857.\n"); exit(1); }
+  if(retval != RIG_OK) { fprintf(stderr, "*** Could not oien NETRIGCTL.\n"); exit(1); }
 
 //  fprintf(stderr, "*** EVERYTHING OKAY 1\n");
 
@@ -59,42 +80,32 @@ int main(int argc, char **argv)
   printf("==========================");
   printf("\n");
 
-  rs_rig_read_state(riga, &statea);
-  rs_rig_read_state(rigb, &stateb);
-  if(memcmp(&statea, &stateb, sizeof(struct rs_rig_state)))
-  {
-    rs_rig_write_state(riga, &stateb);
-  }
-  rs_rig_state_cmp(&statep, &statea);
+  memset(&statea, 0, sizeof(struct rs_rig_state));
+  memset(&stateb, 0, sizeof(struct rs_rig_state));
+  memset(&stateap, 0, sizeof(struct rs_rig_state));
+  memset(&statebp, 0, sizeof(struct rs_rig_state));
 
   for ( ; ; )
   {
     rs_rig_read_state(riga, &statea);
     rs_rig_read_state(rigb, &stateb);
-
+    
+    if(rs_rig_state_cmp(&statea, &stateap))
     {
-      if(rs_rig_state_cmp(&statea, &statep))
-      {
-//        printf("\nrig a changed\n");
-        rs_rig_write_state(rigb, &statea);
-      }
-      else if(rs_rig_state_cmp(&stateb, &statep))
-      {
-//        printf("\n");
-//        printf("rig b changed: b= %'15.1f/%7s/%ld\n", stateb.freq, rig_strrmode(stateb.mode), stateb.width);
-//        printf("               p= %'15.1f/%7s/%ld\n", statep.freq, rig_strrmode(statep.mode), statep.width);
-        rs_rig_write_state(riga, &stateb);
-      }
+      rs_rig_write_state(rigb, &statea);
     }
-
+    else if(rs_rig_state_cmp(&stateb, &statebp))
+    {
+      rs_rig_write_state(riga, &stateb);
+    }
+    rs_rig_read_state(riga, &stateap);
+    rs_rig_read_state(rigb, &statebp);
     printf( "%'15.1f Hz %-7s", statea.freq, rig_strrmode(statea.mode));
     printf(" %'15.1f Hz %-7s", stateb.freq, rig_strrmode(stateb.mode));
     printf("\r");
     fflush(stdout);
-    memcpy(&statep, &statea, sizeof(struct rs_rig_state));
-    msleep(2000);
+    usleep(10000);
   }
-  printf("\n");
 
   rig_close(riga);
   rig_close(rigb);
@@ -131,16 +142,6 @@ void read_args(int argc, char **argv)
 static int _finished = 0;     
 int finished() { return _finished; }
 
-void nsleep(int ns)
-{
-  struct timespec ts;
-  ts.tv_nsec = ns;
-  ts.tv_sec = 0;
-  nanosleep(&ts, NULL);
-}
-
-void msleep(int ms) { return nsleep(ms * 1000000); }
-
 void rs_rig_read_state(RIG *rig, struct rs_rig_state *state)
 {
   rig_get_freq(rig, RIG_VFO_CURR, &(state->freq));
@@ -149,8 +150,10 @@ void rs_rig_read_state(RIG *rig, struct rs_rig_state *state)
 
 void rs_rig_write_state(RIG *rig, struct rs_rig_state *state)
 {
+  struct rs_rig_state curstate;
+  rs_rig_read_state(rig, &curstate);
   rig_set_freq(rig, RIG_VFO_CURR, (state->freq));
-  rig_set_mode(rig, RIG_VFO_CURR, (state->mode), (state->width));
+  rig_set_mode(rig, RIG_VFO_CURR, (state->mode), (curstate.width));
 }
 
 int rs_rig_state_cmp(struct rs_rig_state *statea, struct rs_rig_state *stateb)
@@ -158,5 +161,11 @@ int rs_rig_state_cmp(struct rs_rig_state *statea, struct rs_rig_state *stateb)
   if(statea->freq != stateb->freq) { return 1; }
   if(statea->mode != stateb->mode) { return 2; }
   return 0;
+}
+
+void rs_rig_state_cpy(struct rs_rig_state *state_dest, struct rs_rig_state *state_orig)
+{
+  state_dest->freq = state_orig->freq;
+  state_dest->mode = state_orig->mode;
 }
 
